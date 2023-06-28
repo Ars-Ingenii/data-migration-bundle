c7f60f164f021c9cb3b8fa810d8fee7931c9c97f
alitvinavicius <artur.litvinavicius@nfq.lt>
2023-06-28 19:33:27 +0300

    ARS-2153 updated migration command

--- src/Command/DataMigrationCheckerCommand.php
+++ src/Command/DataMigrationCheckerCommand.php
@@ -20,6 +20,7 @@ use DataMigrationBundle\Entity\DataMigrationInterface;
 use DataMigrationBundle\Factory\DataMigrationFactory;
 use DataMigrationBundle\Repository\DataMigrationRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -31,11 +32,14 @@ class DataMigrationCheckerCommand extends Command
 
     private DataMigrationRepository $dataMigrationRepository;
 
+    private LoggerInterface $logger;
+
     /** @var DataMigrationInterface[] */
     private iterable $dataMigrations;
     public function __construct(
         EntityManagerInterface $entityManager,
         DataMigrationRepository $dataMigrationRepository,
+        LoggerInterface $logger,
         iterable $dataMigrations
     )
     {
@@ -44,6 +48,7 @@ class DataMigrationCheckerCommand extends Command
         $this->entityManager = $entityManager;
         $this->dataMigrationRepository = $dataMigrationRepository;
         $this->dataMigrations = $dataMigrations;
+        $this->logger = $logger;
     }
 
     protected function configure(): void
@@ -55,25 +60,38 @@ class DataMigrationCheckerCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        foreach ($this->dataMigrations as $dataMigration) {
-            if ($dataMigration instanceof DataMigrationInterface) {
-                $label = $dataMigration->getLabel();
-                $record = $this->dataMigrationRepository->findOneBy(['label' => $label]);
+        try {
+            foreach ($this->dataMigrations as $dataMigration) {
+                if ($dataMigration instanceof DataMigrationInterface) {
+                    $label = $dataMigration->getLabel();
+                    $record = $this->dataMigrationRepository->findOneBy(['label' => $label]);
 
-                if ($this->validate($label) === false) {
-                    $output->writeln('Migration with label ' . $label . ' has been skipped. Bad naming.');
-                    continue;
-                }
+                    if ($this->validate($label) === false) {
+                        $output->writeln('Migration with label ' . $label . ' has been skipped. Bad naming.');
+                        continue;
+                    }
 
-                if ($record === null) {
-                    $newDataMigrationData = DataMigrationFactory::create($dataMigration->getLabel(), get_class($dataMigration));
-                    $this->entityManager->persist($newDataMigrationData);
+                    if ($record === null) {
+                        $newDataMigrationData = DataMigrationFactory::create($dataMigration->getLabel(), get_class($dataMigration));
+                        $this->entityManager->persist($newDataMigrationData);
+                    }
                 }
             }
+
+            $this->entityManager->flush();
+        } catch (\Exception $exception) {
+            $this->logger->error(
+                'Data migration checker error occurred.',
+                [
+                    'message' => $exception->getMessage()
+                ]
+            );
+            $output->writeln('Exception occurred. ' . $exception->getMessage());
+
+            return self::FAILURE;
         }
 
-        $this->entityManager->flush();
-        return 1;
+        return self::SUCCESS;
     }
 
     private function validate(string $label): bool
--- src/Command/DataMigrationCommand.php
+++ src/Command/DataMigrationCommand.php
@@ -16,13 +16,41 @@ declare(strict_types=1);
 
 namespace DataMigrationBundle\Command;
 
+use DataMigrationBundle\Entity\DataMigration;
+use DataMigrationBundle\Entity\DataMigrationInterface;
+use DataMigrationBundle\Repository\DataMigrationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class DataMigrationCommand extends Command
+class DataMigrationCommand extends Command implements ContainerAwareInterface
 {
     protected static $defaultName = 'ars:migrate:data';
+
+    protected ContainerInterface $container;
+
+    private EntityManagerInterface $entityManager;
+
+    private DataMigrationRepository $dataMigrationRepository;
+
+    private LoggerInterface $logger;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        DataMigrationRepository $dataMigrationRepository,
+        LoggerInterface $logger
+    )
+    {
+        parent::__construct();
+
+        $this->entityManager = $entityManager;
+        $this->dataMigrationRepository = $dataMigrationRepository;
+        $this->logger = $logger;
+    }
 
     protected function configure(): void
     {
@@ -33,6 +61,39 @@ class DataMigrationCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        return 0;
+        try {
+            $dataMigrations = $this->dataMigrationRepository->findBy(['executed' => false]);
+
+            if (count($dataMigrations) === 0) {
+                $output->writeln('No data migrations to execute.');
+
+                return self::SUCCESS;
+            }
+
+            /** @var DataMigration $migration */
+            foreach ($dataMigrations as $migration) {
+                /** @var DataMigrationInterface $service */
+                $service = $this->container->get($migration->getLabel());
+                $service->migrate();
+            }
+
+        } catch (\Exception $exception) {
+            $this->logger->error(
+                'Data migration error occurred.',
+                [
+                    'message' => $exception->getMessage()
+                ]
+            );
+            $output->writeln('Exception occurred. ' . $exception->getMessage());
+
+            return self::FAILURE;
+        }
+
+        return self::SUCCESS;
+    }
+
+    public function setContainer(?ContainerInterface $container)
+    {
+        $this->container = $container;
     }
 }
